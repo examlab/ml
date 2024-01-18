d7a325a9f00365ab4b071d0e56c56c11b65a9914
examlab <157038610+examlab@users.noreply.github.com>
2024-01-18 21:17:11 +0530

    Backprop.py

--- Backprop.py
+++ Backprop.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix,precision_score,recall_score
+
+
 m=pd.read_csv('document.csv',names=['messagge','label'])
 print("Total instance of data set:",m.shape[0])
 m['labelnum']=ms.label.map({'pos':1,'neg':0})
@@ -10,13 +12,19 @@ x=m.message
 y=m.labelnum
 Xtrain,Xtest,ytrain,ytest=train_test_split(X,y)
 count_v=coutVectorizer()
+
+
 Xtrain_dtm=count_v.fit_transform(Xtrain)
 Xtest_dtm=count_v.fit(transform(Xtest))
 df=pd.DataFrame(Xtrain_dtm.toarray(),columns=count_v.get_feature_names_out())
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(Xtrain_dm, ytrain)
+
+
 pred = clf.predict(Xtest_dm)
 print(pred)
+
+
 print('Accuracy: ',accuracy_score(y,pred))
 print('Confusion matrix: ',confusion_matrix(y,pred))
 print('Precision: ',precision_score(y,pred))
