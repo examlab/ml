a01a9b0aeb60b7b634c3dbd486167c8cea7a92d5
examlab <157038610+examlab@users.noreply.github.com>
2024-01-19 08:29:40 +0530

    Logreg.py

--- Logreg.py
+++ Logreg.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn.linear_model import LogisticRegression
-import sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix
 
 
-x=numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
-y=numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+x=np.array([3.78,2.44,2.09,0.14,1.72,1.65,4.92,4.37,4.96,4.52,3.69,5.88]).reshape(-1,1)
+y=np.array([0,0,0,0,0,0,1,1,1,1,1,1])
 
 
 model=LogisticRegression(solver='liblinear',random_state=0)
