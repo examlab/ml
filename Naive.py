a502664a0fcdeae2406cb488ba55191cc890b3e3
examlab <157038610+examlab@users.noreply.github.com>
2024-01-19 08:01:54 +0530

    Naive.py

--- Naive.py
+++ Naive.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score,confusion_matrix,precision_score,reca
 
 
 msg=pd.read_csv('document.csv',names=['message','label'])
-print("Total instance of data set:",m.shape[0])
+print("Total instance of data set:",msg.shape[0])
 msg['labelnum']=msg.label.map({'pos':1,'neg':0})
 X=msg.message
 y=msg.labelnum
@@ -15,15 +15,15 @@ Xtrain,Xtest,ytrain,ytest=train_test_split(X,y)
 count_v=CountVectorizer()
 
 
-Xtrain_dtm=count_v.fit_transform(Xtrain)
-Xtest_dtm=count_v.fit(transform(Xtest))
+Xtrain_dm=count_v.fit_transform(Xtrain)
+Xtest_dm=count_v.transform(Xtest)
 df=pd.DataFrame(Xtrain_dtm.toarray(),columns=count_v.get_feature_names_out())
 clf=MultinomialNB()
-clf.fit(Xtrain_dtm,ytrain)
+clf.fit(Xtrain_dm,ytrain)
 MultinomialNB()
 
 
-pred=clf.predict(Xtest_dtm)
+pred=clf.predict(Xtest_dm)
 print(pred)
 
 
@@ -31,5 +31,3 @@ print('Accuracy: ',accuracy_score(y,pred))
 print('Confusion matrix: ',confusion_matrix(y,pred))
 print('Precision: ',precision_score(y,pred))
 print('Recall: ',recall_score(y,pred))
-
-
